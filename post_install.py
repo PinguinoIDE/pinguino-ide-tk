3d1f918c6c6b423f8da82eb5d8aa23d4cb60bdae
YeisonEng <yeison.eng@gmail.com>
2014-10-23 15:48:03 -0500

    Update

---
+++ post_install.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import sys
+import shutil
+
+from qtgui.pinguino_api.pinguino_config import PinguinoConfig
+
+os.environ["PINGUINO_HOME"] = os.path.abspath(sys.path[0])
+
+# For PyInstaller compatibility
+if os.path.exists(os.path.abspath("pinguino_data")):
+    os.environ["PINGUINO_DATA"] = os.path.abspath("pinguino_data")
+else:
+    os.environ["PINGUINO_DATA"] = os.getenv("PINGUINO_HOME")
+
+PinguinoConfig.set_environ_vars()
+
+#Remove files and directories
+if os.path.isdir(os.path.join(os.environ["PINGUINO_USER_PATH"], "source")):
+    shutil.rmtree(os.path.join(os.environ["PINGUINO_USER_PATH"], "source"))
+
+#Check files and directories
+PinguinoConfig.check_user_files()
+
+#Remove library names
+os.remove(os.path.join(os.environ["PINGUINO_USER_PATH"], "reserved.pickle"))

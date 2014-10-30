a90984737f9a04c9c7b0245f6458252b5d1b510d
YeisonEng <yeison.eng@gmail.com>
2014-10-30 01:39:36 -0500

    Update paths dialog.

--- tkgui/ide/child_windows/paths.py
+++ tkgui/ide/child_windows/paths.py
@@ -22,7 +22,7 @@ class Paths(Frame):
         self.parent = master
         self.main = main
 
-        self.parent.geometry("640x270")
+        self.parent.geometry("640x150")
         self.parent.title(os.getenv("NAME")+" - Paths")
 
         self.master.configure(padx=10, pady=10)
@@ -32,28 +32,28 @@ class Paths(Frame):
         icon = os.path.join("tkgui", "resources", "icons", "clear.png")
         image_clear = ImageTk.PhotoImage(file=icon)
 
-        lb8 = LabelFrame(self.parent, text="8-bit")
-        frame8 = Frame(lb8)
-        frame8.pack(fill=X, expand=True, side=TOP)
+        # lb8 = LabelFrame(self.parent, text="8-bit")
+        # frame8 = Frame(self.parent)
+        # self.parent.pack(fill=X, expand=True, side=TOP)
 
-        frame8c = Frame(frame8)
+        frame8c = Frame(self.parent)
         frame_text1 = Frame(frame8c)
         frame_text1.pack(side=LEFT)
-        Label(frame_text1, text="SDCC compiler:", width=width, anchor="w").pack(side=LEFT)
-        self.label_sdcc_bin = Entry(frame8c)
-        self.label_sdcc_bin.pack(side=LEFT, fill=X, expand=True)
+        # Label(frame_text1, text="SDCC compiler:", width=width, anchor="w").pack(side=LEFT)
+        # self.label_sdcc_bin = Entry(frame8c)
+        # self.label_sdcc_bin.pack(side=LEFT, fill=X, expand=True)
         frame8c.pack(fill=X, expand=True, side=TOP)
 
-        button = Button(frame8c, image=image_clear, relief=FLAT, command=lambda :self.reset_value("sdcc_bin"), borderwidth=0, highlightthickness=0)
-        button.image = image_clear
-        button.pack(side=LEFT, padx=2, pady=4)
+        # button = Button(frame8c, image=image_clear, relief=FLAT, command=lambda :self.reset_value("sdcc_bin"), borderwidth=0, highlightthickness=0)
+        # button.image = image_clear
+        # button.pack(side=LEFT, padx=2, pady=4)
 
-        Button(frame8c, text="Change...", command=self.set_dir_dialog(self.label_sdcc_bin)).pack(side=LEFT)
+        # Button(frame8c, text="Change...", command=self.set_dir_dialog(self.label_sdcc_bin)).pack(side=LEFT)
 
-        frame8l = Frame(frame8)
+        frame8l = Frame(self.parent)
         frame_text2 = Frame(frame8l)
         frame_text2.pack(side=LEFT)
-        Label(frame_text2, text="Libraries:", width=width, anchor="w").pack(side=LEFT)
+        Label(frame_text2, text="8-bit libraries:", width=width, anchor="w").pack(side=LEFT)
         self.label_pinguino_8_libs = Entry(frame8l)
         self.label_pinguino_8_libs.pack(side=LEFT, fill=X, expand=True)
 
@@ -66,28 +66,28 @@ class Paths(Frame):
         frame8l.pack(fill=X, expand=True, side=TOP)
 
 
-        lb32 = LabelFrame(self.parent, text="32-bit")
+        # lb32 = LabelFrame(self.parent, text="32-bit")
 
-        frame32 = Frame(lb32)
+        # frame32 = Frame(lb32)
 
 
-        frame32c = Frame(frame32)
+        frame32c = Frame(self.parent)
         frame_text3 = Frame(frame32c)
         frame_text3.pack(side=LEFT)
-        Label(frame_text3, text="GCC compiler:", width=width, anchor="w").pack(side=LEFT)
-        self.label_gcc_bin = Entry(frame32c)
-        self.label_gcc_bin.pack(side=LEFT, fill=X, expand=True)
-        button = Button(frame32c, image=image_clear, relief=FLAT, command=lambda :self.reset_value("gcc_bin"), borderwidth=0, highlightthickness=0)
-        button.image = image_clear
-        button.pack(side=LEFT, padx=2, pady=4)
-        Button(frame32c, text="Change...", command=self.set_dir_dialog(self.label_gcc_bin)).pack(side=LEFT)
+        # Label(frame_text3, text="GCC compiler:", width=width, anchor="w").pack(side=LEFT)
+        # self.label_gcc_bin = Entry(frame32c)
+        # self.label_gcc_bin.pack(side=LEFT, fill=X, expand=True)
+        # button = Button(frame32c, image=image_clear, relief=FLAT, command=lambda :self.reset_value("gcc_bin"), borderwidth=0, highlightthickness=0)
+        # button.image = image_clear
+        # button.pack(side=LEFT, padx=2, pady=4)
+        # Button(frame32c, text="Change...", command=self.set_dir_dialog(self.label_gcc_bin)).pack(side=LEFT)
         frame32c.pack(fill=X, expand=True, side=TOP)
 
 
-        frame32l = Frame(frame32)
+        frame32l = Frame(self.parent)
         frame_text4 = Frame(frame32l)
         frame_text4.pack(side=LEFT)
-        Label(frame_text4, text="Libraries:", width=width, anchor="w").pack(side=LEFT)
+        Label(frame_text4, text="32-bit libraries:", width=width, anchor="w").pack(side=LEFT)
         self.label_pinguino_32_libs = Entry(frame32l)
         self.label_pinguino_32_libs.pack(side=LEFT, fill=X, expand=True)
         button = Button(frame32l, image=image_clear, relief=FLAT, command=lambda :self.reset_value("pinguino_32_libs"), borderwidth=0, highlightthickness=0)
@@ -97,11 +97,11 @@ class Paths(Frame):
         frame32l.pack(fill=X, expand=True, side=TOP)
 
 
-        frame32.pack(fill=X, expand=True, side=TOP)
+        # self.parent.pack(fill=X, expand=True, side=TOP)
 
 
-        lb8.pack(fill=X, expand=True, side=TOP, padx=10, pady=10)
-        lb32.pack(fill=X, expand=True, side=TOP, padx=10, pady=10)
+        # lb8.pack(fill=X, expand=True, side=TOP, padx=10, pady=10)
+        # lb32.pack(fill=X, expand=True, side=TOP, padx=10, pady=10)
 
 
 
@@ -109,15 +109,11 @@ class Paths(Frame):
         Button(self.master, text="Close", command=self.quit).pack(side=RIGHT)
 
 
-        self.dialog_dirs = ((self.label_gcc_bin, "GCC_BIN"),
-                            (self.label_sdcc_bin, "SDCC_BIN"),
-                            (self.label_pinguino_8_libs, "PINGUINO_8_LIBS"),
+        self.dialog_dirs = ((self.label_pinguino_8_libs, "PINGUINO_8_LIBS"),
                             (self.label_pinguino_32_libs, "PINGUINO_32_LIBS"),
                            )
 
-        default_paths = {"SDCC_BIN": self.main.configIDE.get_path("sdcc_bin"),
-                         "GCC_BIN": self.main.configIDE.get_path("gcc_bin"),
-                         "PINGUINO_8_LIBS": self.main.configIDE.get_path("pinguino_8_libs"),
+        default_paths = {"PINGUINO_8_LIBS": self.main.configIDE.get_path("pinguino_8_libs"),
                          "PINGUINO_32_LIBS": self.main.configIDE.get_path("pinguino_32_libs"),
                          }
 
